cc18fac22125508f1c47a7171df5d1f84d651f34
waldla <laura.walden@aalto.fi>
2016-11-20 16:33:39 +0200

    rock'n' roll map

---
+++ rockData.js
@@ -0,0 +1,683 @@
+var decades = [
+	{"name":"2040s",
+		"countries": [
+			{"name":"United States",
+				"count":40,
+				"provinces": [
+					{"name":"Mississippi",
+						"count":6,
+						"examples":[
+							{"name":"John Lee Hooker"},
+							{"name":"Joseph Lee Williams"}
+						]
+					},
+					{"name":"Alabama",
+						"count":4,
+						"examples":[
+							{"name":"Pinetop Smith"},
+							{"name":"Delmore Brothers"}
+						]
+					},
+					{"name":"Lousiana",
+						"count":4,
+						"examples":[
+							{"name":"Lead Belly"},
+							{"name":""}
+						]
+					},
+					{"name":"Arkansas",
+						"count":3,
+						"examples":[
+							{"name":"Sister Rosetta Tharpe"},
+							{"name":"Louis Jordan & His Typmany Five"}
+						]
+					},
+					{"name":"California",
+						"count":2,
+						"examples":[
+							{"name":"Ella Fitzgerald"},
+							{"name":"Big Joe Turner and Pete Johnson"}
+						]
+					},
+					{"name":"Georgia",
+						"count":2,
+						"examples":[
+							{"name":"Trixie Smith"},
+							{"name":"Billy Ward & His Dominoes"}
+						]
+					},
+					{"name":"Illinois",
+						"count":2,
+						"examples":[
+							{"name":"Tampa Red"},
+							{"name":"Benny Goodman"}
+						]
+					},
+					{"name":"Nebraska",
+						"count":2,
+						"examples":[
+							{"name":"Wynonie Harris"},
+							{"name":"Paul Williams"}
+						]
+					},
+					{"name":"Oklahoma",
+						"count":2,
+						"examples":[
+							{"name":"Joe Liggins"},
+							{"name":"The Honeydrippers"}
+						]
+					},
+					{"name":"Texas",
+						"count":2,
+						"examples":[
+							{"name":"Goree Carter"},
+							{"name":"Freddie Slack & Ella Mae Morse"}
+						]
+					},
+					{"name":"Kentucky",
+						"count":1,
+						"examples":[
+							{"name":"Freddie Slack & Ella Mae Morse"},
+							{"name":"Goree Carter"}
+						]
+					},
+					{"name":"Michigan",
+						"count":1,
+						"examples":[
+							{"name":"Wild Bill Moore"},
+							{"name":""}
+						]
+					},
+					{"name":"New Jersey",
+						"count":1,
+						"examples":[
+							{"name":"Will Bradley"},
+							{"name":""}
+						]
+					},
+						{"name":"New Orleans",
+						"count":1,
+						"examples":[
+							{"name":"Champion Jack Dupre"},
+							{"name":""}
+						]
+					},
+					{"name":"Ohio",
+						"count":1,
+						"examples":[
+							{"name":"Tiny Bradshaw"},
+							{"name":""}
+						]
+					},
+					{"name":"Pennsylvania",
+						"count":1,
+						"examples":[
+							{"name":"Jimmy Preston"},
+							{"name":""}
+						]
+					},
+					{"name":"New York",
+						"count":1,
+						"examples":[
+							{"name":"Louis Armstrong-Corona"},
+							{"name":""}
+						]
+					},
+					{"name":"South Carolina",
+						"count":1,
+						"examples":[
+							{"name":"Arthur Smith"},
+							{"name":""}
+						]
+					},
+					{"name":"Tennessee",
+						"count":1,
+						"examples":[
+							{"name":"Stick McGhee & His Buddies"},
+							{"name":""}
+						]
+					},
+					{"name":"Virginia",
+						"count":1,
+						"examples":[
+							{"name":"Golden Gate Quartet"},
+							{"name":""}
+						]
+					},
+					{"name":"Washington",
+						"count":1,
+						"examples":[
+							{"name":"Duke Ellington"},
+							{"name":""}
+						]
+					}
+				]}
+				]
+	},
+	//-----------------------------------------------------
+	{"name":"50s",
+		"countries": [
+			{"name":"United States",
+				"count":40,
+				"provinces": [
+					{"name":"Lousiana",
+						"count":4,
+						"examples":[
+							{"name":"Fats Domino"},
+							{"name":"Jerry Lee Lewis"}
+						]
+					},
+					{"name":"California",
+						"count":3,
+						"examples":[
+							{"name":"The Coasters"},
+							{"name":"The Platters"}
+						]
+					},
+					{"name":"Georgia",
+						"count":3,
+						"examples":[
+							{"name":"Chuck Willis"},
+							{"name":"Little Richard"}
+						]
+					},
+					{"name":"Illinois",
+						"count":3,
+						"examples":[
+							{"name":"Sam Cooke"},
+							{"name":"The Flamingos"}
+						]
+					},
+					{"name":"New York",
+						"count":3,
+						"examples":[
+							{"name":"The Dominoes"},
+							{"name":"The Drifters"}
+						]
+					},
+					{"name":"Texas",
+						"count":3,
+						"examples":[
+							{"name":"Buddy Holly & The Crickets"},
+							{"name":"Jesse Belvin"}
+						]
+					},
+					{"name":"Michigan",
+						"count":2,
+						"examples":[
+							{"name":"Hank Ballard & The Midnighters"},
+							{"name":"Jackie Wilson"}
+						]
+					},
+					{"name":"Mississippi",
+						"count":2,
+						"examples":[
+							{"name":"Elvis Presley"},
+							{"name":"Bo Diddley"}
+						]
+					},
+					{"name":"Missouri",
+						"count":2,
+						"examples":[
+							{"name":"Chuck Berry"},
+							{"name":"Big Joe Turner"}
+						]
+					},
+					{"name":"North Carolina",
+						"count":2,
+						"examples":[
+							{"name":"The 5 Royales"},
+							{"name":"Clyde McPhatter"}
+						]
+					},
+					{"name":"Tennessee",
+						"count":2,
+						"examples":[
+							{"name":"Johnny Ace"},
+							{"name":"Carl Perkins"}
+						]
+					},
+					{"name":"Virginia",
+						"count":2,
+						"examples":[
+							{"name":"Ruth Brown"},
+							{"name":"Gene Vincent & The Blue Caps"}
+						]
+					},
+					{"name":"Arkansas",
+						"count":1,
+						"examples":[
+							{"name":"Little Willie John"},
+							{"name":""}
+						]
+					},
+					{"name":"Florida",
+						"count":1,
+						"examples":[
+							{"name":"Ray Charles"},
+							{"name":""}
+						]
+					},
+					{"name":"Indiana",
+						"count":1,
+						"examples":[
+							{"name":"The Spaniels"},
+							{"name":""}
+						]
+					},
+					{"name":"Iowa",
+						"count":1,
+						"examples":[
+							{"name":"The Everly Brothers"},
+							{"name":""}
+						]
+					},
+					{"name":"Minnesota",
+						"count":1,
+						"examples":[
+							{"name":"Eddie Cochran"},
+							{"name":""}
+						]
+					},
+					{"name":"New Jersey",
+						"count":1,
+						"examples":[
+							{"name":"Ricky Nelson"},
+							{"name":""}
+						]
+					},
+					{"name":"Ohio",
+						"count":1,
+						"examples":[
+							{"name":"The Moonglows"},
+							{"name":""}
+						]
+					},
+					{"name":"Pennsylvania",
+						"count":1,
+						"examples":[
+							{"name":"Bill Haley & The Comets"},
+							{"name":""}
+						]
+					},
+					{"name":"Washington",
+						"count":1,
+						"examples":[
+							{"name":"The Clovers"},
+							{"name":""}
+						]
+					}
+				]},
+				]
+	},
+	//---------------------------------------
+	{"name":"60s",
+		"countries": [
+			{"name":"United States",
+				"count":34,
+				"provinces": [
+					{"name":"California",
+						"count":8,
+						"examples":[
+							{"name":"Creedence Clearwater Revival"},
+							{"name":"The Doors"}
+						]
+					},
+					{"name":"Michigan",
+						"count":6,
+						"examples":[
+							{"name":"Stevie Wonder"},
+							{"name":"The Temptations"}
+						]
+					},
+					{"name":"Tennessee",
+						"count":4,
+						"examples":[
+							{"name":"The Impressions"},
+							{"name":"Sam & Dave"}
+						]
+					},
+					{"name":"New Jersey",
+						"count":3,
+						"examples":[
+							{"name":"The Four Seasons"},
+							{"name":"The Shirelles"}
+						]
+					},
+					{"name":"Georgia",
+						"count":2,
+						"examples":[
+							{"name":"Ray Charles"},
+							{"name":"Otis Redding"}
+						]
+					},
+					{"name":"Mississippi",
+						"count":2,
+						"examples":[
+							{"name":"Elvis Presley"},
+							{"name":"Sam Cooke"}
+						]
+					},
+					{"name":"New York",
+						"count":2,
+						"examples":[
+							{"name":"The Drifters"},
+							{"name":"Dion"}
+						]
+					},
+					{"name":"Washington",
+						"count":2,
+						"examples":[
+							{"name":"The Jimi Hendrix Experience"},
+							{"name":"Marvin Gaye"}
+						]
+					},
+					{"name":"Alabama",
+						"count":1,
+						"examples":[
+							{"name":"Wilson Pickett"},
+							{"name":""}
+						]
+					},
+					{"name":"Minnesota",
+						"count":1,
+						"examples":[
+							{"name":"Bob Dylan"},
+							{"name":""}
+						]
+					},
+					{"name":"Pennsylvania",
+						"count":1,
+						"examples":[
+							{"name":"Solomon Burke"},
+							{"name":""}
+						]
+					},
+					{"name":"South Carolina",
+						"count":1,
+						"examples":[
+							{"name":"James Brown"},
+							{"name":""}
+						]
+					},
+					{"name":"Texas",
+						"count":1,
+						"examples":[
+							{"name":"Roy Orbison"},
+							{"name":""}
+						]
+					}
+				]},
+				{"name":"United Kingdom",
+					"count":6,
+					"provinces": [
+						{"name":"England",
+							"count":6,
+							"examples":[
+								{"name":"Led Zeppelin"},
+								{"name":"Cream"}
+							]
+						}
+					]}
+				]
+	},
+		//----------------------------------
+		{"name":"70s",
+			"countries": [
+				{"name":"United States",
+					"count":23,
+					"provinces": [
+						{"name":"Illinois",
+							"count":3,
+							"examples":[
+								{"name":"Chicago"},
+								{"name":"Curtis Mayfield"}
+							]
+						},
+						{"name":"New Jersey",
+							"count":3,
+							"examples":[
+								{"name":"Parliament/Funkadelic"},
+								{"name":"Bruce Springsteen"}
+							]
+						},
+						{"name":"California",
+							"count":2,
+							"examples":[
+								{"name":"The Eagles"},
+								{"name":"Bob Dylan"}
+							]
+						},
+						{"name":"Florida",
+							"count":2,
+							"examples":[
+								{"name":"Lynyrd Skynyrd"},
+								{"name":"The Allman Brothers Band"}
+							]
+						},
+						{"name":"New York",
+							"count":2,
+							"examples":[
+								{"name":"KISS"},
+								{"name":"The Ramones"}
+							]
+						},
+						{"name":"Ohio",
+							"count":2,
+							"examples":[
+								{"name":"The Isley Brothers"},
+								{"name":"The O'Jays"}
+							]
+						},
+						{"name":"Arkansas",
+							"count":1,
+							"examples":[
+								{"name":"Al Green"},
+								{"name":""}
+							]
+						},
+						{"name":"Georgia",
+							"count":1,
+							"examples":[
+								{"name":"Gladys Knight & The Pips"},
+								{"name":""}
+							]
+						},
+						{"name":"Indiana",
+							"count":1,
+							"examples":[
+								{"name":"The Jackson 5"},
+								{"name":""}
+							]
+						},
+						{"name":"Massachusetts",
+							"count":1,
+							"examples":[
+								{"name":"Aerosmith"},
+								{"name":""}
+							]
+						},
+						{"name":"Michigan",
+							"count":1,
+							"examples":[
+								{"name":"Stevie Wonder"},
+								{"name":"Alice Cooper"}
+							]
+						},
+						{"name":"South Carolina",
+							"count":1,
+							"examples":[
+								{"name":"James Brown"},
+								{"name":""}
+							]
+						},
+						{"name":"Tennessee",
+							"count":1,
+							"examples":[
+								{"name":"Aretha Franklin"},
+								{"name":""}
+							]
+						},
+						{"name":"Washington",
+							"count":1,
+							"examples":[
+								{"name":"Marvin Gaye"},
+								{"name":""}
+							]
+						}
+					]},
+					{"name":"United Kingdom",
+						"count":14,
+						"provinces": [
+							{"name":"England",
+								"count":14,
+								"examples":[
+									{"name":"David Bowie"},
+									{"name":"Queen"}
+								]
+							}
+						]},
+						{"name":"Australia",
+							"count":1,
+							"provinces": [
+								{"name":"Queensland",
+									"count":1,
+									"examples":[
+										{"name":"Bee Gees"},
+										{"name":""}
+									]
+								}
+							]},
+							{"name":"Canada",
+								"count":2,
+								"provinces": [
+									{"name":"Ontario",
+										"count":2,
+										"examples":[
+											{"name":"Neil Young"},
+											{"name":"Rush"}
+										]
+									}
+								]}
+							]
+		},
+//-------------------------------------
+{"name":"80s",
+	"countries": [
+		{"name":"United States",
+			"count":26,
+			"provinces": [
+				{"name":"New York",
+					"count":7,
+					"examples":[
+						{"name":"Public Enemy"},
+						{"name":"Billy Joel"}
+					]
+				},
+				{"name":"California",
+					"count":6,
+					"examples":[
+						{"name":"Guns N' Roses"},
+						{"name":"Metallica"}
+					]
+				},
+				{"name":"New Jersey",
+					"count":4,
+					"examples":[
+						{"name":"Bruce Springsteen"},
+						{"name":"Whitney Houston"}
+					]
+				},
+				{"name":"Indiana",
+					"count":3,
+					"examples":[
+						{"name":"Michael Jackson"},
+						{"name":"Janet Jackson"}
+					]
+				},
+				{"name":"Alabama",
+					"count":1,
+					"examples":[
+						{"name":"Lionel Richie"},
+						{"name":""}
+					]
+				},
+				{"name":"Georgia",
+					"count":2,
+					"examples":[
+						{"name":"R.E.M."},
+						{"name":""}
+					]
+				},
+				{"name":"Illinois",
+					"count":1,
+					"examples":[
+						{"name":"REO Speedwagon"},
+						{"name":""}
+					]
+				},
+				{"name":"Michigan",
+					"count":1,
+					"examples":[
+						{"name":"Madonna"},
+						{"name":""}
+					]
+				},
+				{"name":"Minnesota",
+					"count":1,
+					"examples":[
+						{"name":"Prince"},
+						{"name":""}
+					]
+				},
+				{"name":"Tennessee",
+					"count":1,
+					"examples":[
+						{"name":"Tina Turner"},
+						{"name":""}
+					]
+				}
+			]},
+			{"name":"United Kingdom",
+				"count":12,
+				"provinces": [
+					{"name":"England",
+						"count":11,
+						"examples":[
+							{"name":"Dire Straits"},
+							{"name":"Phil Collins"}
+						]
+					},
+					{"name":"Ireland",
+						"count":1,
+						"examples":[
+							{"name":"U2"},
+							{"name":""}
+						]
+					}
+				]},
+				{"name":"Australia",
+					"count":1,
+					"provinces": [
+						{"name":"New South Wales",
+							"count":1,
+							"examples":[
+								{"name":"AC/DC"},
+								{"name":""}
+							]
+						}
+					]},
+					{"name":"Canada",
+						"count":1,
+						"provinces": [
+							{"name":"Ontario",
+								"count":1,
+								"examples":[
+									{"name":"Rush"},
+									{"name":""}
+								]
+							}
+						]}
+					]
+}
+]
---
+++ rockScript.js
@@ -0,0 +1,332 @@
+	google.load('visualization', '1', {
+		'packages': ['geochart']
+	});
+	google.setOnLoadCallback(init);
+
+	var options;
+	var data, worldData, usData, ukData, auData, caData;
+  var chart;
+  var dataFocus = 'world';
+  var dataIdent = ['world', 'US', 'GB', 'AU', 'CA'];
+  var zoom = 'countries';
+  var decIndex = 0;
+
+  function init(){
+
+        // Define init options
+        options = {
+        	backgroundColor: {
+        		fill: 'rgb(250,250,250)',
+        		stroke: '#FFFFFF',
+        		strokeWidth: 0
+        	},
+        	colorAxis: {
+            colors: ['#5592AE','#0F1E26']
+          },  
+          //legend:['#7AC5CD','#000033'],
+          datalessRegionColor: 'RGB(230,230,240)',
+          enableRegionInteractivity: 'true',
+        //Choose the map view (zoomed in or not)
+        region: dataFocus,
+        resolution: zoom,
+        keepAspectRatio: true,
+        width: 800,
+        //height: 500,
+        tooltip: {
+         textStyle: {color: '#444444'},
+         trigger: 'hover',
+         isHtml: true,
+         legend: 'none'
+        }
+      };
+
+      loadData();
+      setWorld();
+      addListeners();
+      initVisualization();
+
+    }
+//---------------------------------------------------------------------------------------------------------
+function loadData(){
+    //world data
+    worldData = new google.visualization.DataTable();
+    worldData.addColumn('string', 'Country');
+    worldData.addColumn('number', 'Value');
+    // A column for custom tooltip content
+    worldData.addColumn({type:'string', role:'tooltip'});
+
+    for(i=0;i<decades[decIndex].countries.length;i++){
+      var countryName=decades[decIndex].countries[i].name;
+      var percentage=decades[decIndex].countries[i].count/40*100;
+      //one decimal
+      var p = percentage.toFixed();
+      var artistCountWorld=decades[decIndex].countries[i].count;
+      //v recognizes the geograp. area, v is the title row
+      worldData.addRows([[{v:countryName,f:countryName + ", " + p + '%'},percentage,""]]);       
+    }
+
+    //US data
+    usData = new google.visualization.DataTable();
+    usData.addColumn('string', 'Country');
+    usData.addColumn('number', 'Value');
+        // A column for custom tooltip content
+    usData.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
+
+          //US data
+          for(i=0;i<decades[decIndex].countries[0].provinces.length;i++){
+          	var stateName=decades[decIndex].countries[0].provinces[i].name;
+          	var artistCount=decades[decIndex].countries[0].provinces[i].count;
+
+            var percentage=(decades[decIndex].countries[0].provinces[i].count)/(decades[decIndex].countries[0].count)*100;
+            var p = percentage.toFixed();
+
+            var artistName1=decades[decIndex].countries[0].provinces[i].examples[0].name;
+            var artistName2=decades[decIndex].countries[0].provinces[i].examples[1].name;
+
+            if(decades[decIndex].countries[0].provinces[i].examples[1].name==""){
+              usData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent2(artistName1)]]);
+            }
+            else{
+              usData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent(artistName1, artistName2)]]);    
+            }   
+          }
+
+//---------------------------------------------------------------------------------------------------------
+          //UK data
+
+          if(decades[decIndex].countries.length > 1){
+
+          	ukData = new google.visualization.DataTable();
+          	ukData.addColumn('string', 'Country');
+          	ukData.addColumn('number', 'Value');
+          	ukData.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
+
+          	for(i=0;i<decades[decIndex].countries[1].provinces.length;i++){
+          		var stateName=decades[decIndex].countries[1].provinces[i].name;
+          		var artistCount=decades[decIndex].countries[1].provinces[i].count;
+
+              var percentage=(decades[decIndex].countries[1].provinces[i].count)/(decades[decIndex].countries[1].count)*100;
+              var p = percentage.toFixed();
+
+              var artistName1=decades[decIndex].countries[1].provinces[i].examples[0].name;
+              var artistName2=decades[decIndex].countries[1].provinces[i].examples[1].name;
+
+              if(decades[decIndex].countries[1].provinces[i].examples[1].name==""){
+                ukData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent2(artistName1)]]);
+              }
+              else{
+                ukData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent(artistName1, artistName2)]]);    
+              }   
+            }
+          }
+
+
+//---------------------------------------------------------------------------------------------------------
+          // Au data
+          if(decades[decIndex].countries.length > 2){
+          	auData = new google.visualization.DataTable();
+          	auData.addColumn('string', 'Country');
+          	auData.addColumn('number', 'Value');
+          	auData.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
+
+          	for(i=0;i<decades[decIndex].countries[2].provinces.length;i++){
+          		var stateName=decades[decIndex].countries[2].provinces[i].name;
+          		var artistCount=decades[decIndex].countries[2].provinces[i].count;
+
+              var percentage=(decades[decIndex].countries[2].provinces[i].count)/(decades[decIndex].countries[2].count)*100;
+              var p = percentage.toFixed();
+
+              var artistName1=decades[decIndex].countries[2].provinces[i].examples[0].name;
+              var artistName2=decades[decIndex].countries[2].provinces[i].examples[1].name;
+
+              if(decades[decIndex].countries[2].provinces[i].examples[1].name==""){
+                auData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent2(artistName1)]]);
+              }
+              else{
+                auData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent(artistName1, artistName2)]]);    
+              }   
+            }
+          }
+//---------------------------------------------------------------------------------------------------------
+            // Canada data
+            if(decades[decIndex].countries.length > 3){
+            	caData = new google.visualization.DataTable();
+            	caData.addColumn('string', 'Country');
+            	caData.addColumn('number', 'Value');
+            	caData.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
+
+            	for(i=0;i<decades[decIndex].countries[3].provinces.length;i++){
+            		var stateName=decades[decIndex].countries[3].provinces[i].name;
+            		var artistCount=decades[decIndex].countries[3].provinces[i].count;
+
+                var percentage=(decades[decIndex].countries[3].provinces[i].count)/(decades[decIndex].countries[3].count)*100;
+                var p = percentage.toFixed();
+
+                var artistName1=decades[decIndex].countries[3].provinces[i].examples[0].name;
+                var artistName2=decades[decIndex].countries[3].provinces[i].examples[1].name;
+
+                if(decades[decIndex].countries[3].provinces[i].examples[1].name==""){
+                  caData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent2(artistName1)]]);
+                }
+                else{ 
+                  caData.addRows([[{v:stateName,f:stateName + ', ' + p + '%'},percentage,createCustomHTMLContent(artistName1, artistName2)]]);    
+                }   
+
+              }
+            }
+
+//--------------------------------------------------------------------------------------------------------
+switch (options.region) {
+  case "world":
+  data = worldData;
+  break;
+  case "US":
+  data = usData;
+  break;
+  case "GB":
+  if(decades[decIndex].countries.length > 1){
+    data = ukData;
+  } else {
+    data = worldData;
+  }
+  break;
+  case "AU":
+  if(decades[decIndex].countries.length > 2){
+    data = auData;
+  } else {
+    data = worldData;
+  }
+  break;
+  case "CA":
+  if(decades[decIndex].countries.length > 3){
+    data = caData;
+  } else {
+    data = worldData;
+  }
+  break;
+  default:
+            //
+          }
+        }
+//---------------------------------------------------------------------------------------------------------
+
+// Add button listeners
+function addListeners() {
+  document.getElementById("globe").addEventListener("click", function(){
+    setWorld();
+    chart.draw(data, options);
+  });
+  document.getElementById("2040s").addEventListener("click", function(){
+    decIndex = 0;
+    loadData();
+    chart.draw(data, options);
+  });
+  document.getElementById("50s").addEventListener("click", function(){
+    decIndex = 1;
+    loadData();
+    chart.draw(data, options);
+  });
+  document.getElementById("60s").addEventListener("click", function(){
+    decIndex = 2;
+    loadData();
+    chart.draw(data, options);
+  });
+  document.getElementById("70s").addEventListener("click", function(){
+    decIndex = 3;
+    loadData();
+    chart.draw(data, options);
+  });
+  document.getElementById("80s").addEventListener("click", function(){
+    decIndex = 4;
+    loadData();
+    chart.draw(data, options);
+  });
+}
+
+//---------------------------------------------------------------------------------------------------------
+/*function createCustomHTMLContent(imageURL, description) {
+return '<div style="padding:5px 5px 5px 5px;">' +
+'<img src="' + imageURL + '" style="width:100px;height:70px"><br/>' + description;
+}*/
+
+function createCustomHTMLContent(artist1, artist2) {
+  return '<div style="margin-right:-10px; font-size:11px;">' + 'Artist Highlights' + '<br>' + '<ul>' + '<li>' + artist1 + '</li>' + '<li>' + artist2 + '</li>' + '</ul>';
+}
+
+function createCustomHTMLContent2(artist1) {
+  return '<div style="margin-right:-10px; font-size:11px;">' + 'Artist Highlight' + '<br>' + '<ul>' + '<li>' + artist1 + '</li>';
+}
+
+//---------------------------------------------------------------------------------------------------------
+
+function setWorld() {
+	options.region = 'world';
+	options.resolution = 'countries';
+  data = worldData;
+}
+//---------------------------------------------------------------------------------------------------------
+function setUS() {
+	options.region = 'US';
+	options.resolution = 'provinces';
+        //options.displayMode = 'provinces';
+        data = usData;
+      }
+//---------------------------------------------------------------------------------------------------------
+function setUK() {
+	options.region = 'GB';
+	options.resolution = 'provinces';
+	data = ukData;
+}
+//---------------------------------------------------------------------------------------------------------
+function setAU() {
+	options.region = 'AU';
+	options.resolution = 'provinces';
+	data = auData;
+}
+
+//---------------------------------------------------------------------------------------------------------
+function setCA() {
+	options.region = 'CA';
+	options.resolution = 'provinces';
+	data = caData;
+}
+//---------------------------------------------------------------------------------------------------------
+
+function initVisualization() {  
+	chart = new google.visualization.GeoChart(document.getElementById('visualization'));
+	google.visualization.events.addListener(chart, 'select', selectHandler);
+	chart.draw(data, options);
+
+      // The select handler. Call the chart's getSelection() method
+      function selectHandler() {
+      	/*Returns an array of the selected chart entities. Selectable entities are regions with an assigned value. For this chart, only one entity can be selected at any given moment.*/
+      	var selection = chart.getSelection();
+
+      	if (selection.length == 1 & options.region == 'world') {
+      		var selectedRow = selection[0].row;
+          //getValue(rowIndex, columnIndex)
+          var selectedRegion = data.getValue(selectedRow, 0);
+
+          if (selectedRow == 0) {
+            setUS();
+            chart.draw(data, options);
+          }
+          else if (selectedRow == 1) {
+            setUK();
+            chart.draw(data, options);
+          }
+          else if (selectedRow == 2) {
+            setAU();
+            chart.draw(data, options);
+          }
+          else {
+            setCA();
+            chart.draw(data, options);
+          }
+        }
+      }
+    }
+
+ /*document.getElementById("clickme").addEventListener("click", function(){
+  alert("click");
+});*/
